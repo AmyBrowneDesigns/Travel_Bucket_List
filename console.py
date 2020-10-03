3c107660422d840d3f03e536e860c8d7f26fc220
AmyBrowneDesigns <amybrowne20@gmail.com>
2020-10-03 13:08:53 +0100

    created db and sql file for psql

--- console.py
+++ console.py
@@ -1,1 +1,35 @@
 import pdb
+from models.city import City
+from models.country import Country
+
+import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
+
+
+city_repository.delete_all()
+country_repository.delete_all()
+
+country1 = Country("Singapore")
+country_repository.save(country1)
+country2 = Country("UAE")
+country_repository.save(country2)
+country3 = Country("Argentina")
+country_repository.save(country3)
+country4 = Country("China")
+country_repository.save(country4)
+
+country_repository.select_all()
+
+city1 = City("Singapore", False)
+city_repository.save(city1)
+city2 = City(" Abu Dhabi", False)
+city_repository.save(city2)
+city3 = City("Buenos Aires", False)
+city_repository.save(city3)
+city4 = City("Shangai", False)
+city_repository.save(city4)
+
+# city_repository.select_all() is this needed at the end?
+
+
+pdb.set_trace()
